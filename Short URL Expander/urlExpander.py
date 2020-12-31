3bd383d9864ae7301b692f6bf5a4471ab1571365
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2021-01-01 06:22:06 +0800

    Add files via upload

---
+++ Short URL Expander/urlExpander.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3 
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+if(len(sys.argv) != 3):	
+	print("format: python3 urlExpander.py <mode> <short url>", end='\n\n')	
+	print("Mode:")
+	print("1) bit.ly")
+	print("2) others")
+	sys.exit(0)
+
+shortURL = sys.argv[2]
+mode = int(sys.argv[1])
+
+if(mode == 1):
+	#bit.ly exposed the real website location in the HTTP header 
+	rsl = requests.head(shortURL)
+	expandUrl = rsl.headers['Location']
+	print("Expand URL: " + expandUrl)
+elif(mode == 2):
+	#use expander website	
+	expanderLink = "http://urlexpander.net"
+	data = {
+		"url" : shortURL
+	}
+	rsl = requests.post(expanderLink, data=data)	
+	soup = BeautifulSoup(rsl.text, 'html.parser')
+	retLinks = soup.findAll("a", {"class": "result-a"})
+	expandUrl = str(retLinks[1]).split()[2].split('=')[1].replace('"', '')
+	print("Expand URL: " + expandUrl)
+else:
+	print("Please Select the Correct Mode.")
+
+
